50d96e86d6d9d93921a424c7b5e1104ca896daed
Owen Yoshishige <owenyoshishige@gmail.com>
2022-04-24 19:14:36 -0700

    Refractored ETL script with complete psql commands so ETL can be ran in one sql file run.

--- server/schemas/ETL.sql
+++ server/schemas/ETL.sql
@@ -1,69 +1,75 @@
+--NOTE: You will have to go into the csv files and edit the
+--table names to match the csv temp tables being created below
+--The column names can be changed by changing the first line of the csv files.
+
 -- Create Tables to import CSV files to
--- CREATE TABLE questionscsv (
---   question_id varchar,
---   product_id varchar,
---   question_body varchar(1000),
---   question_date bigINT,
---   asker_name varchar(60),
---   reported boolean,
---   question_helpfulness int,
---   email varchar(60)
--- );
+CREATE TABLE questionscsv (
+  question_id varchar,
+  product_id varchar,
+  question_body varchar(1000),
+  question_date bigINT,
+  asker_name varchar(60),
+  reported boolean,
+  question_helpfulness int,
+  email varchar(60)
+);
 
--- CREATE TABLE answerscsv (
---   id bigint,
---   question_id varchar,
---   body varchar(1000),
---   date bigINT,
---   answerer_name varchar(60),
---   reported boolean,
---   helpfulness int,
---   email varchar(60)
--- );
+CREATE TABLE answerscsv (
+  id bigint,
+  question_id varchar,
+  body varchar(1000),
+  date bigINT,
+  answerer_name varchar(60),
+  reported boolean,
+  helpfulness int,
+  email varchar(60)
+);
 
--- CREATE TABLE photoscsv (
---   photo_id int,
---   answer_id bigint,
---   photo_URL varchar
--- );
+CREATE TABLE photoscsv (
+  photo_id int,
+  answer_id bigint,
+  photo_URL varchar
+);
 
--- Copy the csv contents to those tables
--- COPY questionscsv(
---   question_id,
---   product_id,
---   question_body,
---   question_date,
---   asker_name,
---   email,
---   reported,
---   question_helpfulness
--- )
--- FROM '/Users/owenyoshishige/Documents/Hack_Reactor_SEI/SDC/ice-climbers-qa-api/csvfiles/questions.csv'
--- DELIMITER ','
--- CSV HEADER;
 
--- COPY answerscsv(
---   id,
---   question_id,
---   body,
---   date,
---   answerer_name,
---   email,
---   reported,
---   helpfulness
--- )
--- FROM '/Users/owenyoshishige/Documents/Hack_Reactor_SEI/SDC/ice-climbers-qa-api/csvfiles/answers.csv'
--- DELIMITER ','
--- CSV HEADER;
+-- Copy all CSV Data into the temp tables
+Copy the csv contents to those tables
+COPY questionscsv(
+  question_id,
+  product_id,
+  question_body,
+  question_date,
+  asker_name,
+  email,
+  reported,
+  question_helpfulness
+)
+FROM '/Users/owenyoshishige/Documents/Hack_Reactor_SEI/SDC/ice-climbers-qa-api/csvfiles/questions.csv'
+DELIMITER ','
+CSV HEADER;
 
--- COPY photoscsv(
---   photo_id,
---   answer_id,
---   photo_url
--- )
--- FROM '/Users/owenyoshishige/Documents/Hack_Reactor_SEI/SDC/ice-climbers-qa-api/csvfiles/answers_photos.csv'
--- DELIMITER ','
--- CSV HEADER;
+COPY answerscsv(
+  id,
+  question_id,
+  body,
+  date,
+  answerer_name,
+  email,
+  reported,
+  helpfulness
+)
+FROM '/Users/owenyoshishige/Documents/Hack_Reactor_SEI/SDC/ice-climbers-qa-api/csvfiles/answers.csv'
+DELIMITER ','
+CSV HEADER;
+
+COPY photoscsv(
+  photo_id,
+  answer_id,
+  photo_url
+)
+FROM '/Users/owenyoshishige/Documents/Hack_Reactor_SEI/SDC/ice-climbers-qa-api/csvfiles/answers_photos.csv'
+DELIMITER ','
+CSV HEADER;
 
 
 -- Insert all of the csv table data into our real tables
@@ -149,7 +155,25 @@ SELECT
 FROM (SELECT * from photoscsv) i
 ON CONFLICT DO NOTHING;
 
+
+--Add Auto Increment to main tables
+CREATE SEQUENCE question_id_sq;
+ALTER TABLE questions
+ALTER COLUMN question_id SET DEFAULT nextval('question_id_sq');
+SELECT setval('question_id_sq', (SELECT MAX(question_id::integer) FROM questions) + 1);
+
+CREATE SEQUENCE answer_id_sq;
+ALTER TABLE answers
+ALTER COLUMN id SET DEFAULT nextval('answer_id_sq');
+SELECT setval('answer_id_sq', (SELECT MAX(id::integer) FROM answers) + 1);
+
 CREATE SEQUENCE photo_id_sq;
 ALTER TABLE photos
 ALTER COLUMN photo_id SET DEFAULT nextval('photo_id_sq');
 SELECT setval('photo_id_sq', (SELECT MAX(photo_id) FROM photos) + 1);
+
+
+--Drop temp tables
+DROP TABLE IF EXISTS questionscsv;
+DROP TABLE IF EXISTS answerscsv;
+DROP TABLE IF EXISTS photoscsv;
