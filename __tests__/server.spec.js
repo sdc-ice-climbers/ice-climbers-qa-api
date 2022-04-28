b045597624aee3c77fc934baa07ba06ce64a051a
Owen Yoshishige <owenyoshishige@gmail.com>
2022-04-28 00:29:11 -0700

    fix edge cases and build testing suite

--- __tests__/server.spec.js
+++ __tests__/server.spec.js
@@ -1,36 +1,40 @@
 require('dotenv').config();
 const pactum = require('pactum');
-const URL = `http://localhost:${process.env.PORT}/`;
-
+const stub = require('./stubs/stubs.js');
 let timeout = 100000;
 
+beforeAll(() => {
+  pactum.request.setBaseUrl(`http://${process.env.DB_HOST}:${process.env.PORT}/`);
+  jest.setTimeout(timeout);
+});
+
 describe('GET all questions',() => {
-  jest.setTimeout(timeout)
+
   test('Should return status 200 when no count or page are provided', async function () {
     await pactum.spec()
       .withRequestTimeout(timeout)
-      .get(URL + 'qa/questions?product_id=1000')
+      .get('qa/questions?product_id=1000')
       .expectStatus(200)
   });
 
   test('Should return status 200 when count and page are provided', async function () {
     await pactum.spec()
       .withRequestTimeout(timeout)
-      .get(URL + 'qa/questions?count=5&page=1&product_id=1')
+      .get('qa/questions?count=5&page=1&product_id=1')
       .expectStatus(200)
   });
 
   test('Should return status 422 when no product id is provided', async function () {
     await pactum.spec()
       .withRequestTimeout(timeout)
-      .get(URL + 'qa/questions/')
+      .get('qa/questions/')
       .expectStatus(422)
   });
 
   test('Results should be empty on page 2 with count of 5', async function () {
     await pactum.spec()
       .withRequestTimeout(timeout)
-      .get(URL + 'qa/questions?count=5&page=2&product_id=1')
+      .get('qa/questions?count=5&page=2&product_id=1')
       .expectJson({
         "product_id": "1",
         "results": []
@@ -39,16 +43,108 @@ describe('GET all questions',() => {
 });
 
 describe('GET all answers',() => {
-  jest.setTimeout(timeout)
-  test.only('Should return status 200', async function () {
+
+  test('Should return status 200', async function () {
     await pactum.spec()
-      .get(URL + 'qa/questions/1000/answers')
+      .get('qa/questions/1000/answers')
       .expectStatus(200)
   })
 
-  test.only('Should return status 404 when sending in wrong question id', async function () {
+  test('Should return status 422 when sending in wrong question id', async function () {
     await pactum.spec()
-      .get(URL + 'qa/questions/blah/answers')
-      .expectStatus(404)
+      .get('qa/questions/blah/answers')
+      .expectStatus(422)
+  })
+});
+
+describe('POST questions',() => {
+
+  test('Should return status 204 when sending in a body', async function () {
+    await pactum.spec()
+      .post('qa/questions/')
+      .withBody(stub.goodQuestion)
+      .expectStatus(204)
+  })
+
+  test('Should return status 204 when sending in no body', async function () {
+    await pactum.spec()
+      .post('qa/questions/')
+      .expectStatus(422)
+  })
+});
+
+describe('POST answers',() => {
+
+  test('Should return status 204 when sending in a body', async function () {
+    await pactum.spec()
+      .post('qa/questions/1000/answers')
+      .withBody(stub.goodAnswer)
+      .expectStatus(204)
+  })
+
+  test('Should return status 422 when sending no body', async function () {
+    await pactum.spec()
+      .post('qa/questions/1000/answers')
+      .expectStatus(422)
+  })
+});
+
+describe('PUT increase question helpfulness',() => {
+
+  test('Should return status 204 sending correct question id', async function () {
+    await pactum.spec()
+      .put('qa/questions/59825/helpful')
+      .expectStatus(204)
+  })
+
+  test('Should return status 422 when sending bad question id', async function () {
+    await pactum.spec()
+      .put('qa/questions/5678908765789/helpful')
+      .expectStatus(422)
+  })
+});
+
+describe('PUT increase answer helpfulness',() => {
+
+  test('Should return status 204 sending correct answer id', async function () {
+    await pactum.spec()
+      .put('qa/answers/39328/helpful')
+      .expectStatus(204)
+  })
+
+  test('Should return status 422 when sending bad answer id', async function () {
+    await pactum.spec()
+      .put('qa/answers/3923435523328/helpful')
+      .expectStatus(422)
+  })
+});
+
+describe('PUT report question',() => {
+
+  test('Should return status 204 sending correct question id', async function () {
+    await pactum.spec()
+      .put('qa/questions/35969/report')
+      .expectStatus(204)
+  })
+
+  test('Should return status 422 when sending bad question id', async function () {
+    await pactum.spec()
+      .put('qa/questions/3518912312369/report')
+      .expectStatus(422)
+  })
+});
+
+describe('PUT report answer',() => {
+
+  test('Should return status 204 sending correct answer id', async function () {
+    await pactum.spec()
+      .put('qa/answers/8/report')
+      .expectStatus(204)
+  })
+
+  test('Should return status 422 when sending bad answer id', async function () {
+    await pactum.spec()
+      .put('qa/answers/82143468976543/report')
+      .expectStatus(422)
   })
 });
---
+++ __tests__/stubs/stubs.js
@@ -0,0 +1,21 @@
+let goodAnswer = {
+  body: 'This yellow colored clothing is great!!1',
+  name: 'Mr Banana',
+  email: 'MrBanana@gmail.com',
+  photos: [
+      'https://images.unsplash.com/photo-1511127088257-53ccfcc769fa?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80',
+      'https://images.unsplash.com/photo-1517720359744-6d12f8a09b10?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1567&q=80'
+  ]
+};
+
+let goodQuestion = {
+  body: 'Is there a yellow camo?',
+  name: 'Mr Banana',
+  email: 'MrBanana@gmail.com',
+  product_id: '100000'
+};
+
+module.exports = {
+  goodAnswer,
+  goodQuestion
+};
